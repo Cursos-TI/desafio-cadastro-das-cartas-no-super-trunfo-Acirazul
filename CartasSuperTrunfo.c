6300b94ff7bd90f44c207dc2937ad4ddfacd4a95
acirazul <gerziakelly@hotmail.com>
2025-05-23 00:44:13 +0000

    test

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,37 +5,40 @@ int main() {
 
     // Declaração das variáveis
     char codigo_da_cidade;
-    char nome_da_cidade[20];
+    char nome_da_cidade[50];
     int populacao;
     float area;
     float PIB;
     int quantidade_pontos_turisticos;
 
+    // Novas variáveis calculadas
     float densidade_populacional;
-    float pib_per_capita; // Novas variáveis calculadas
+    float pib_per_capita;
 
-    printf("Digite o código da cidade: \n");
-    scanf(" %c", &codigo_da_cidade);  // Entrada de dados
+    // Entrada de dados
+    printf("\nDigite o código da cidade: ");
+    scanf(" %c", &codigo_da_cidade);
 
-    printf("Digite o nome da cidade: \n");
+    printf("Digite o nome da cidade: ");
     scanf(" %[^\n]", nome_da_cidade);
 
-    printf("Digite a população: \n");
+    printf("Digite a população: ");
     scanf("%d", &populacao);
 
-    printf("Digite a área (em km²): \n");
+    printf("Digite a área (em km²): ");
     scanf("%f", &area);
 
-    printf("Digite o PIB (em milhões): \n");
+    printf("Digite o PIB (em milhões): ");
     scanf("%f", &PIB);
 
-    printf("Digite a quantidade de pontos turísticos: \n");
+    printf("Digite a quantidade de pontos turísticos: ");
     scanf("%d", &quantidade_pontos_turisticos);
-   
-    densidade_populacional = populacao / area;
-    pib_per_capita = PIB / populacao;  // Cálculos
 
-    
+    // Cálculos
+    densidade_populacional = populacao / area;
+    pib_per_capita = PIB / populacao;
+
+    // Saída de dados
     printf("\n--- Dados da Cidade Cadastrada ---\n");
     printf("Código da Cidade: %c\n", codigo_da_cidade);
     printf("Nome da Cidade: %s\n", nome_da_cidade);
@@ -44,7 +47,7 @@ int main() {
     printf("PIB da Cidade: %.2f milhões\n", PIB);
     printf("Quantidade de Pontos Turísticos: %d\n", quantidade_pontos_turisticos);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional);
-    printf("PIB per Capita: %.2f\n", pib_per_capita); // Saída de dados
+    printf("PIB per Capita: %.2f\n", pib_per_capita);
 
     return 0;
 }
